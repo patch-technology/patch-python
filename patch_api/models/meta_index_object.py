557567224bf3d60f5fe8e8781e12bea34d3fc0eb
Thiago Araujo <thd.araujo@gmail.com>
2021-02-16 10:32:14 -0800

    Revert "wip"
    This reverts commit 35d6948dafe5f99308c50f93d4ef37723ea86d75.

--- patch_api/models/meta_index_object.py
+++ patch_api/models/meta_index_object.py
@@ -33,17 +33,13 @@ class MetaIndexObject(object):
       attribute_map (dict): The key is attribute name
                             and the value is json key in definition.
     """
-    openapi_types = {
-        'prev_page': 'int',
-        'next_page': 'int'
-    }
+    openapi_types = {"prev_page": "int", "next_page": "int"}
 
-    attribute_map = {
-        'prev_page': 'prev_page',
-        'next_page': 'next_page'
-    }
+    attribute_map = {"prev_page": "prev_page", "next_page": "next_page"}
 
-    def __init__(self, prev_page=None, next_page=None, local_vars_configuration=None):  # noqa: E501
+    def __init__(
+        self, prev_page=None, next_page=None, local_vars_configuration=None
+    ):  # noqa: E501
         """MetaIndexObject - a model defined in OpenAPI"""  # noqa: E501
         if local_vars_configuration is None:
             local_vars_configuration = Configuration()
@@ -105,18 +101,20 @@ class MetaIndexObject(object):
         for attr, _ in six.iteritems(self.openapi_types):
             value = getattr(self, attr)
             if isinstance(value, list):
-                result[attr] = list(map(
-                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
-                    value
-                ))
+                result[attr] = list(
+                    map(lambda x: x.to_dict() if hasattr(x, "to_dict") else x, value)
+                )
             elif hasattr(value, "to_dict"):
                 result[attr] = value.to_dict()
             elif isinstance(value, dict):
-                result[attr] = dict(map(
-                    lambda item: (item[0], item[1].to_dict())
-                    if hasattr(item[1], "to_dict") else item,
-                    value.items()
-                ))
+                result[attr] = dict(
+                    map(
+                        lambda item: (item[0], item[1].to_dict())
+                        if hasattr(item[1], "to_dict")
+                        else item,
+                        value.items(),
+                    )
+                )
             else:
                 result[attr] = value
 
