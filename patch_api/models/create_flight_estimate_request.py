2d989c5841a12e3da755df0d6860009495ab22b9
Paul Cothenet <pcothenet@gmail.com>
2021-09-07 11:04:40 -0700

    Add new flights parameters (#36)
    * Add new flights
    
    * Update changelog

--- patch_api/models/create_flight_estimate_request.py
+++ patch_api/models/create_flight_estimate_request.py
@@ -33,10 +33,24 @@ class CreateFlightEstimateRequest(object):
       attribute_map (dict): The key is attribute name
                             and the value is json key in definition.
     """
-    openapi_types = {"distance_m": "int", "project_id": "str", "create_order": "bool"}
+    openapi_types = {
+        "distance_m": "int",
+        "origin_airport": "str",
+        "destination_airport": "str",
+        "aircraft_code": "str",
+        "cabin_class": "str",
+        "passenger_count": "int",
+        "project_id": "str",
+        "create_order": "bool",
+    }
 
     attribute_map = {
         "distance_m": "distance_m",
+        "origin_airport": "origin_airport",
+        "destination_airport": "destination_airport",
+        "aircraft_code": "aircraft_code",
+        "cabin_class": "cabin_class",
+        "passenger_count": "passenger_count",
         "project_id": "project_id",
         "create_order": "create_order",
     }
@@ -44,6 +58,11 @@ class CreateFlightEstimateRequest(object):
     def __init__(
         self,
         distance_m=None,
+        origin_airport=None,
+        destination_airport=None,
+        aircraft_code=None,
+        cabin_class=None,
+        passenger_count=None,
         project_id=None,
         create_order=None,
         local_vars_configuration=None,
@@ -54,11 +73,21 @@ class CreateFlightEstimateRequest(object):
         self.local_vars_configuration = local_vars_configuration
 
         self._distance_m = None
+        self._origin_airport = None
+        self._destination_airport = None
+        self._aircraft_code = None
+        self._cabin_class = None
+        self._passenger_count = None
         self._project_id = None
         self._create_order = None
         self.discriminator = None
 
         self.distance_m = distance_m
+        self.origin_airport = origin_airport
+        self.destination_airport = destination_airport
+        self.aircraft_code = aircraft_code
+        self.cabin_class = cabin_class
+        self.passenger_count = passenger_count
         self.project_id = project_id
         self.create_order = create_order
 
@@ -81,12 +110,6 @@ class CreateFlightEstimateRequest(object):
         :type: int
         """
         if (
-            self.local_vars_configuration.client_side_validation and distance_m is None
-        ):  # noqa: E501
-            raise ValueError(
-                "Invalid value for `distance_m`, must not be `None`"
-            )  # noqa: E501
-        if (
             self.local_vars_configuration.client_side_validation
             and distance_m is not None
             and distance_m > 400000000
@@ -104,6 +127,111 @@ class CreateFlightEstimateRequest(object):
             )  # noqa: E501
 
         self._distance_m = distance_m
+
+    @property
+    def origin_airport(self):
+        """Gets the origin_airport of this CreateFlightEstimateRequest.  # noqa: E501
+
+
+        :return: The origin_airport of this CreateFlightEstimateRequest.  # noqa: E501
+        :rtype: str
+        """
+        return self._origin_airport
+
+    @origin_airport.setter
+    def origin_airport(self, origin_airport):
+        """Sets the origin_airport of this CreateFlightEstimateRequest.
+
+
+        :param origin_airport: The origin_airport of this CreateFlightEstimateRequest.  # noqa: E501
+        :type: str
+        """
+
+        self._origin_airport = origin_airport
+
+    @property
+    def destination_airport(self):
+        """Gets the destination_airport of this CreateFlightEstimateRequest.  # noqa: E501
+
+
+        :return: The destination_airport of this CreateFlightEstimateRequest.  # noqa: E501
+        :rtype: str
+        """
+        return self._destination_airport
+
+    @destination_airport.setter
+    def destination_airport(self, destination_airport):
+        """Sets the destination_airport of this CreateFlightEstimateRequest.
+
+
+        :param destination_airport: The destination_airport of this CreateFlightEstimateRequest.  # noqa: E501
+        :type: str
+        """
+
+        self._destination_airport = destination_airport
+
+    @property
+    def aircraft_code(self):
+        """Gets the aircraft_code of this CreateFlightEstimateRequest.  # noqa: E501
+
+
+        :return: The aircraft_code of this CreateFlightEstimateRequest.  # noqa: E501
+        :rtype: str
+        """
+        return self._aircraft_code
+
+    @aircraft_code.setter
+    def aircraft_code(self, aircraft_code):
+        """Sets the aircraft_code of this CreateFlightEstimateRequest.
+
+
+        :param aircraft_code: The aircraft_code of this CreateFlightEstimateRequest.  # noqa: E501
+        :type: str
+        """
+
+        self._aircraft_code = aircraft_code
+
+    @property
+    def cabin_class(self):
+        """Gets the cabin_class of this CreateFlightEstimateRequest.  # noqa: E501
+
+
+        :return: The cabin_class of this CreateFlightEstimateRequest.  # noqa: E501
+        :rtype: str
+        """
+        return self._cabin_class
+
+    @cabin_class.setter
+    def cabin_class(self, cabin_class):
+        """Sets the cabin_class of this CreateFlightEstimateRequest.
+
+
+        :param cabin_class: The cabin_class of this CreateFlightEstimateRequest.  # noqa: E501
+        :type: str
+        """
+
+        self._cabin_class = cabin_class
+
+    @property
+    def passenger_count(self):
+        """Gets the passenger_count of this CreateFlightEstimateRequest.  # noqa: E501
+
+
+        :return: The passenger_count of this CreateFlightEstimateRequest.  # noqa: E501
+        :rtype: int
+        """
+        return self._passenger_count
+
+    @passenger_count.setter
+    def passenger_count(self, passenger_count):
+        """Sets the passenger_count of this CreateFlightEstimateRequest.
+
+
+        :param passenger_count: The passenger_count of this CreateFlightEstimateRequest.  # noqa: E501
+        :type: int
+        """
+
+        self._passenger_count = passenger_count
 
     @property
     def project_id(self):
