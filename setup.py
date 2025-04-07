ba5c15473bcd4d0517437efae7c7256c4ce9dc5f
Brett Holt <brett@usepatch.com>
2025-04-07 10:25:15 -0700

    Remove /estimates/ecommerce endpoint calls

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "patch-api"
-VERSION = "2.3.0"
+VERSION = "2.3.1"
 # To install the library, run the following
 #
 # python setup.py install
